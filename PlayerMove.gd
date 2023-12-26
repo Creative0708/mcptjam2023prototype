eb60451f885ae560c0a59754a926a20ef8fafeb5
Colin Cai <therealcreative0708@gmail.com>
2023-12-26 11:50:12 -0500

    add basic player movement

--- PlayerMove.gd
+++ PlayerMove.gd
@@ -1,28 +1,21 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 600.0
+const VELOCITY_LERP_FAC = 30
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	motion_mode = MOTION_MODE_FLOATING
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var direction = Input.get_vector("left", "right", "up", "down")
+	
+	var target_velocity = direction * SPEED
+	
+	velocity = lerp(velocity, target_velocity, clamp(delta * VELOCITY_LERP_FAC, 0, 1))
 
 	move_and_slide()
