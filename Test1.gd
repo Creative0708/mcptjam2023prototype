3df46cb94bc4543093839103cc2e93b560fc8dfa
Colin Cai <therealcreative0708@gmail.com>
2023-12-26 21:10:35 -0500

    temp commit

--- Test1.gd
+++ Test1.gd
@@ -1,16 +1,15 @@
 extends Sprite2D
+
 @onready var player = $"../Player"
-var sprite
 
+const MOVEMENT_SPEED = 200
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	sprite = Sprite2D.new()
-	sprite.position.x = 5
-	sprite.position.y = 5
+	position.x = 5
+	position.y = 5
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	sprite.position.x = move_toward(sprite.position.x, player.position.x, 500)
-	sprite.position.y = move_toward(sprite.position.y, player.position.y, 500)
+	position.x = move_toward(position.x, player.position.x, MOVEMENT_SPEED * delta)
+	position.y = move_toward(position.y, player.position.y, MOVEMENT_SPEED * delta)
